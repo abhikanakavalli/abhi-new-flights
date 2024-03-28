6b9e6e64a0883773b915a57464feab15cbd7c870
abhikanakavalli <abhikanakavalli98@gmail.com>
2024-03-28 19:53:27 +0530

    Flight service implementation

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -43,62 +43,62 @@ async function createAirplane(req, res) {
  * POST : /airplanes
  * req-body {}
  */
-// async function getAirplanes(req, res) {
-//     try {
-//         const airplanes = await AirplaneService.getAirplanes();
-//         SuccessResponse.data = airplanes;
-//         return res
-//                 .status(StatusCodes.OK)
-//                 .json(SuccessResponse);
-//     } catch(error) {
-//         ErrorResponse.error = error;
-//         return res
-//                 .status(error.statusCode)
-//                 .json(ErrorResponse);
-//     }
-// }
+async function getAirplanes(req, res) {
+    try {
+        const airplanes = await AirplaneService.getAirplanes();
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
 
 // /**
 //  * POST : /airplanes/:id 
 //  * req-body {}
 //  */
-// async function getAirplane(req, res) {
-//     try {
-//         const airplanes = await AirplaneService.getAirplane(req.params.id);
-//         SuccessResponse.data = airplanes;
-//         return res
-//                 .status(StatusCodes.OK)
-//                 .json(SuccessResponse);
-//     } catch(error) {
-//         ErrorResponse.error = error;
-//         return res
-//                 .status(error.statusCode)
-//                 .json(ErrorResponse);
-//     }
-// }
+async function getAirplane(req, res) {
+    try {
+        const airplanes = await AirplaneService.getAirplane(req.params.id);
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
 
 // /**
 //  * DELETE : /airplanes/:id
 //  * req-body {}
 //  */
-// async function destroyAirplane(req, res) {
-//     try {
-//         const airplanes = await AirplaneService.destroyAirplane(req.params.id);
-//         SuccessResponse.data = airplanes;
-//         return res
-//                 .status(StatusCodes.OK)
-//                 .json(SuccessResponse);
-//     } catch(error) {
-//         ErrorResponse.error = error;
-//         return res
-//                 .status(error.statusCode)
-//                 .json(ErrorResponse);
-//     }
-// }
+async function destroyAirplane(req, res) {
+    try {
+        const airplanes = await AirplaneService.destroyAirplane(req.params.id);
+        SuccessResponse.data = airplanes;
+        return res
+                .status(StatusCodes.OK)
+                .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse);
+    }
+}
 
 module.exports = {
     createAirplane,
-    // getAirplanes,
-    // getAirplane,
-    // destroyAirplane
+    getAirplanes,
+    getAirplane,
+    destroyAirplane
 }
